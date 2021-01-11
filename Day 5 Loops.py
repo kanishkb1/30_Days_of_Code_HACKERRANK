3d92224fc2006b6d98d864561740937376969b69
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2021-01-11 09:34:55 -0800

    Add files via upload

---
+++ Day 5 Loops.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    num=10
+    for i in range(num):
+        print((n),"x",(i+1),"=",(n*(i+1)))
+         
